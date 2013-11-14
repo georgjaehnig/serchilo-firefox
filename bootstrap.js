1902a03e7178a4924f32e957dab7211108c3bf9f
Georg Jaehnig <georg@jaehnig.org>
2013-11-14 15:43:01 +0000

    normalize indentation

--- bootstrap.js
+++ bootstrap.js
@@ -4,11 +4,11 @@ const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 
 let engine_details = {
-    name: 						'Serchilo: de.deu',
-    url: 							'http://www.serchilo.net/n/de.deu?query=_searchTerms_',
-		usage_type: 			'n',
-		namespace_path: 	'de.deu',
-		default_keyword: 	''
+  name:             'Serchilo: de.deu',
+  url:               'http://www.serchilo.net/n/de.deu?query=_searchTerms_',
+	usage_type:       'n',
+	namespace_path:   'de.deu',
+	default_keyword:   ''
 };
 
 
@@ -23,89 +23,89 @@ var firstRun = false;
 var selectSearch = false;
 
 function removeObserver() {
-    try {
-        Services.obs.removeObserver(searchObserver, ENGINE_ADDED);
-    }
-    // If we've already removed this observer, ignore the error.
-    catch (e) {}
+  try {
+    Services.obs.removeObserver(searchObserver, ENGINE_ADDED);
+  }
+  // If we've already removed this observer, ignore the error.
+  catch (e) {}
 }
 
 // Observer called after our engine has been successfully added
 function searchObserver(engine, topic, data) {
-    if (data != "engine-added")
-        return;
+  if (data != "engine-added")
+    return;
 
-    engine.QueryInterface(Ci.nsISearchEngine);
-    if (engine.name == engine_details.name) {
-        // Remove our observer now that we're done with it.
-        removeObserver();
+  engine.QueryInterface(Ci.nsISearchEngine);
+  if (engine.name == engine_details.name) {
+    // Remove our observer now that we're done with it.
+    removeObserver();
 
-        // If the engine is not hidden and this is the first run, move
-        // it to the first position in the engine list and select it
-        if (selectSearch && !engine.hidden) {
-            Services.search.moveEngine(engine, 0);
-            Services.search.currentEngine = engine;
-        }
+    // If the engine is not hidden and this is the first run, move
+    // it to the first position in the engine list and select it
+    if (selectSearch && !engine.hidden) {
+      Services.search.moveEngine(engine, 0);
+      Services.search.currentEngine = engine;
     }
+  }
 }
 // Observer topic
 const ENGINE_ADDED = "browser-search-engine-modified";
 
 function startup(data, reason) {
 
-	//dump(unescape(engine_uri));
-	//dump(unescape(engine_uri2));
+  //dump(unescape(engine_uri));
+  //dump(unescape(engine_uri2));
 
-    firstRun = reason == ADDON_INSTALL;
-    // Re-select the search engine if this is the first run
-    // or we're being re-enabled.
-    selectSearch = firstRun || reason == ADDON_ENABLE;
+  firstRun = reason == ADDON_INSTALL;
+  // Re-select the search engine if this is the first run
+  // or we're being re-enabled.
+  selectSearch = firstRun || reason == ADDON_ENABLE;
 
-    // Only add the engine if it doesn't already exist.
-    let engine = Services.search.getEngineByName(engine_details.name);
-    if (engine)
-        searchObserver(engine, engine_details);
-    else {
-        // Register an observer to detect when the engine has been added, if
-        // necessary.
-        if (selectSearch)
-            Services.obs.addObserver(searchObserver, ENGINE_ADDED, false);
+  // Only add the engine if it doesn't already exist.
+  let engine = Services.search.getEngineByName(engine_details.name);
+  if (engine)
+    searchObserver(engine, engine_details);
+  else {
+    // Register an observer to detect when the engine has been added, if
+    // necessary.
+    if (selectSearch)
+      Services.obs.addObserver(searchObserver, ENGINE_ADDED, false);
 
-				let engine_xml = ENGINE_XML_TEMPLATE;
-				engine_xml = engine_xml.replace('{name}', engine_details.name);
-				engine_xml = engine_xml.replace('{usage_type}', engine_details.usage_type);
-				engine_xml = engine_xml.replace('{namespace_path}', engine_details.namespace_path);
-				engine_xml = engine_xml.replace('{default_keyword}', engine_details.default_keyword_parameter);
+		let engine_xml = ENGINE_XML_TEMPLATE;
+		engine_xml = engine_xml.replace('{name}', engine_details.name);
+		engine_xml = engine_xml.replace('{usage_type}', engine_details.usage_type);
+		engine_xml = engine_xml.replace('{namespace_path}', engine_details.namespace_path);
+		engine_xml = engine_xml.replace('{default_keyword}', engine_details.default_keyword_parameter);
 
-				let engine_uri = wrap_xml_into_data_uri(engine_xml);
+		let engine_uri = wrap_xml_into_data_uri(engine_xml);
 
-        Services.search.addEngine(engine_uri, Ci.nsISearchEngine.DATA_XML, null, false);
-				Services.search.currentEngine = engine;
-    }
+		Services.search.addEngine(engine_uri, Ci.nsISearchEngine.DATA_XML, null, false);
+		Services.search.currentEngine = engine;
+  }
 }
 
 function wrap_xml_into_data_uri(xml) {
-	let uri = 'data:text/xml;charset=utf-8,' + escape(xml);
-	return uri;
+  let uri = 'data:text/xml;charset=utf-8,' + escape(xml);
+  return uri;
 }
 
 function shutdown(data, reason) {
-    // Remove our observer, if necessary
-    if (reason != APP_SHUTDOWN)
-        removeObserver();
+  // Remove our observer, if necessary
+  if (reason != APP_SHUTDOWN)
+    removeObserver();
 
-    // Clean up the search engine on uninstall or disabled.
-    if (reason == ADDON_UNINSTALL || reason == ADDON_DISABLE) {
-        let engine = Services.search.getEngineByName(engine_details.name);
-        // Only remove the engine if it appears to be the same one we
-        // added.
-				dump('begin');
-				dump( engine.getSubmission("_searchTerms_").uri.spec);
-				dump('end');
-        //if (engine && engine.getSubmission("_searchTerms_").uri.spec == engine_details.url)
-        if (engine)
-            Services.search.removeEngine(engine);
-    }
+  // Clean up the search engine on uninstall or disabled.
+  if (reason == ADDON_UNINSTALL || reason == ADDON_DISABLE) {
+    let engine = Services.search.getEngineByName(engine_details.name);
+    // Only remove the engine if it appears to be the same one we
+    // added.
+		dump('begin');
+		dump( engine.getSubmission("_searchTerms_").uri.spec);
+		dump('end');
+    //if (engine && engine.getSubmission("_searchTerms_").uri.spec == engine_details.url)
+    if (engine)
+      Services.search.removeEngine(engine);
+  }
 }
 
 function install() {}
